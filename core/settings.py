3f4dd8445d644a89aa0d8cca6db031a0e18e1473
Diyorbektuit <140270731+Diyorbektuit@users.noreply.github.com>
2023-07-24 17:32:02 +0500

    firts commit

--- core/settings.py
+++ core/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
-import os
 
 
 
@@ -81,9 +79,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # settings.py
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
