134e961a4ee34f317edf887869f65b17402e596a
Diyorbektuit <140270731+Diyorbektuit@users.noreply.github.com>
2023-07-24 19:54:50 +0500

    first commit

--- account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-
-urlpatterns = [
-    path("register", views.register_request, name="register"),
-    path("login", views.loginView, name="login"),
-    path("logout", views.logout_view, name="logout")
-]
